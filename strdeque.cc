01503a8cf4fd4b47271f6defa67c62037881ea14
Krzysztof Adamek <squadack@students.mimuw.edu.pl>
2016-10-18 13:42:09 +0200

    dodano kilka asercji 'pos>=0' oraz dodano DBG z nazwami funkcji

--- strdeque.cc
+++ strdeque.cc
@@ -26,35 +26,46 @@ unsigned long strdeque_new()
 
 void strdeque_delete(unsigned long id)
 {
+	DBG("strdeque_delete(" << id << ")");
 	
 }
 
 size_t strdeque_size(unsigned long id)
 {
+	DBG("strdeque_size(" << id << ")");
 	
 }
 
 void strdeque_insert_at(unsigned long id, size_t pos, const char* value)
 {
+	DBG("strdeque_insert_at(" << id << ", " << pos << ", \"" << value << "\")");
+	assert(pos >= 0);
 	
 }
 
 void strdeque_remove_at(unsigned long id, size_t pos)
 {
+	DBG("strdeque_remove_at(" << id << ", " << pos << ")");
+	assert(pos >= 0);
 	
 }
 
 const char* strdeque_get_at(unsigned long id, size_t pos)
 {
+	DBG("strdeque_get_at(" << id << ", " << pos << ")");
+	assert(pos >= 0);
 	
 }
 
 void strdeque_clear(unsigned long id)
 {
+	DBG("strdeque_clear(" << id << ")");
+	assert(pos >= 0);
 	
 }
 
 int strdeque_comp(unsigned long id1, unsigned long id2)
 {
+	DBG("strdeque_comp(" << id1 << ", " << id2 << ")");
 	
 }
