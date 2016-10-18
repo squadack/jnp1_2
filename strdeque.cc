0a0b125560b6aca88ede5a3a3837095691050d0b
Julia Bernad <yb355336@students.mimuw.edu.pl>
2016-10-18 13:45:39 +0200

    proba

--- strdeque.cc
+++ strdeque.cc
@@ -13,8 +13,10 @@
 
 typedef std::deque<std::string>> strdeq;
 
+
 static std::map<unsigned long, strdeq> deque_map;
 static unsigned long counter = 0;
+
 
 unsigned long strdeque_new()
 {
@@ -28,17 +30,35 @@ unsigned long strdeque_new()
 
 void strdeque_delete(unsigned long id)
 {
-	
+	DBG("strdeque_delete");
+	if(deque_map.count(id) > 0){
+	  deque_map.erase(id);
+	  DBG("strdeque_delete: deque of the key " << id << "deleted");
+	} else {
+	DBG("strdeque_delete: deque of the key " << id << "not found");
+	}
 }
 
 size_t strdeque_size(unsigned long id)
 {
-	
+	DBG("strdeque_size");
+	int res = deque_map.count(id);
+	if(res > 0){
+	  DBG("strdeque_size: deque of the key " << id << "have " << res << "elements");
+	  return res;
+	} else {
+	  DBG("strdeque_size: deque of the key " << id << "have no elements");
+	}
 }
 
 void strdeque_insert_at(unsigned long id, size_t pos, const char* value)
 {
 	
+	DBG("strdeque_insert_at");
+	if(deque_map.count(id) > 0 && value != NULL){
+	    std::string str(value);
+	    
+	}
 }
 
 void strdeque_remove_at(unsigned long id, size_t pos)
