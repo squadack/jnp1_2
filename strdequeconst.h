48663cd43690dec8fefb868a20563925d0f07234
Julia Bernad <yb355336@students.mimuw.edu.pl>
2016-10-25 12:51:53 +0200

    Wstawione komentarze z tresci zadania

--- strdequeconst.h
+++ strdequeconst.h
@@ -7,6 +7,9 @@ extern "C" {
 
 const unsigned long CONST_DEQUE = 0;
 
+
+// Zwraca identyfikator pustej kolejki dwustronnej, do której nic nie można
+// wstawiać i z której nic nie można usuwać.
 unsigned long emptystrdeque();
 
 #ifdef __cplusplus
